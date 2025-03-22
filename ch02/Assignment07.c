4e410a837b0ec845b272856f8c4fd60b6084d599
chanmin <jcm5821@gmail.com>
2025-03-23 01:28:13 +0900

    3/23

--- ch02/Assignment07.c
+++ ch02/Assignment07.c
@@ -1,7 +1,27 @@
+//파일명: Assignment07.c
+
+//내용 : 커피 사이즈와 주문 수량을 입력받아서 출력하는 프로그램을 작성하시오. 커피사이즈는 S,T,G 세 가지 문자 중 하나로 입력받는다.
+
+//작성자 : 정찬민
+
+//날짜 : 2025.3.23
+
+//버전 :
+
+
 #define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
 
 int main(void)
 {
-	printf("커피 사이즈(S,T,G)와 주문 수량?");
+	int num;
+	char size;
 
+	printf("커피 사이즈(S,T,G)와 주문 수량?");
+	scanf("%c %d", &size, &num);
+
+	printf("%c 사이즈 %d잔을 주문합니다.\n", size, num);
+
+	return 0;
 }
